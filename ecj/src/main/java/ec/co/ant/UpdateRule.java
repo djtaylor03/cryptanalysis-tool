5ba55caad149450c9ccf7741a265b7b4801fdaee
ArchDan <dtaylor9@sheffield.ac.uk>
2023-03-16 18:27:25 +0000

    Added ECJ into the repository

---
+++ ecj/src/main/java/ec/co/ant/UpdateRule.java
@@ -0,0 +1,23 @@
+/*
+  Copyright 2019 by Sean Luke
+  Licensed under the Academic Free License version 3.0
+  See the file "LICENSE" for more information
+*/
+package ec.co.ant;
+
+import ec.EvolutionState;
+import ec.Setup;
+import ec.Subpopulation;
+import ec.co.ConstructiveIndividual;
+
+import java.util.List;
+
+/**
+ * Defines a rule for updating pheromone values using a list of "ants."
+ *
+ * @author Eric O. Scott
+ */
+public interface UpdateRule extends Setup
+{
+    void updatePheromones(final EvolutionState state, final PheromoneTable pheromones, final List individuals);
+}
