dd41be3e0452a718cd1e6cb334b25f3590dada82
ArchDan <dtaylor9@sheffield.ac.uk>
2023-03-31 17:40:07 +0100

    Implemented permutations function

--- ecj/src/main/java/ec/app/cryptanalysis_tool/SPN.java
+++ ecj/src/main/java/ec/app/cryptanalysis_tool/SPN.java
@@ -2,9 +2,10 @@ package ec.app.cryptanalysis_tool;
 
 
 // TODO Make a hell of a lot more robust. Currently only works specifically for my purposes.
+// TODO remove half of s and p as they're only indexes.
 public class SPN {
-    int[][] substitutions;  // table for substitutions
-    int[][] permutations;   // table for permutations
+    int[] substitutions;  // table for substitutions
+    int[] permutations;   // table for permutations
     int[] subkey;           // key for mixing
     int rounds;             // number of rounds an SPN has
 
@@ -21,12 +22,10 @@ public class SPN {
 
     // constructor for SPN with Heys Cipher Network
     public SPN(int[] k){
-        int[][] s = {{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15},
-                    {14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7}};
+        int[] s = {14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7};
         substitutions = s;
 
-        int[][] p = {{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16},
-                    {1,5,9,13,2,6,10,14,3,7,11,15,4,8,12,16}};
+        int[] p = {0,4,8,12,1,5,9,13,2,6,10,14,3,7,11,15};
         permutations = p;
 
         subkey = k;
@@ -34,42 +33,86 @@ public class SPN {
     }
 
     // blank constructor for SPN
-    public SPN(int[][] s, int[][] p, int[] k, int r){
+    public SPN(int[] s, int[] p, int[] k, int r){
         substitutions = s;
         permutations = p;
         subkey = k;
-        rounds = 4;
+        rounds = r;
     }
 
     // run SPN
     // p is plain text and should be a list of binary values
     /* 
-     * Formatting should be p.shape = (w, r) 
+     * Formatting of p should be (w, r) 
      * w = width of s-box (for heys, 4 bits)
      * r = how many s-boxes the substitution matrix is. length of key should be (rounds + 1)* w bits long
+     * 
+     * NOTE : formatting with rounds is such to keep consistency with description in Hey's Tutorial
      */
     public int[][] runSPN(int[][] p){
         int[][] u = p;     // intermediate cipher text and starts as plaintext
         int[][] c;     // final cipher text
 
         // N-1 whole rounds
-        for ( int i=0; i < this.rounds; i++){
-            u = this.subkeyMix(p,0);
+        for ( int i = 0; i < this.rounds; i++ ){
+            u = this.subkeyMix(p,i);
             u = this.substitue(u);
             u = this.permute(u);
         }
 
         // final round
-        u = this.subkeyMix(u,0);
+        u = this.subkeyMix(u,this.rounds);
         u = this.substitue(u);
-        c = this.subkeyMix(u,0);
+        c = this.subkeyMix(u,this.rounds + 1);
 
         // output cipher text
         return c;
     }
 
+    /*
+     * MAIN METHODS FOR SPN
+     */
 
-    // helper methods
+    // XOR values with subkey, r is round number
+    public int[][] subkeyMix(int[][] t, int r){
+
+        return t;
+    }
+
+    // substitutes a list of intermediate text inputs. Should be 16 in a [4][4] format
+    public int[][] substitue(int[][] t){
+        int[][] u = t;
+        int num;    // temp variable for more readable code
+
+        // TRY / CATCH block for if there is an issue with mismatched substitution lengths and inputs.
+        try{
+            // iterates through each 'chunk' of 4 bits
+            for( int i = 0; i < t.length; i++ ){
+                num = this.substitutions[binaryToInt(t[i])];
+                u[i] = intToBinary(num);
+            }
+        } catch(Exception e){
+            // hopefully catches any issues with mismatched s-box lengths and inputs
+            System.out.println(e);
+        }
+        return u;
+    }
+
+    // permutes a list text inputs 
+    public int[][] permute(int[][] t){     
+        int[][] u = t;
+
+        for ( int i = 0; i < t.length; i++ ){
+            for ( int j = 0; j < t[i].length; j++ ){
+                u[i][j] = this.permutations[i+j];
+            }
+        }
+        return u;
+    }
+
+    /*
+     * HELPER METHODS
+     */
 
     // methods to convert to and from binary
     public int binaryToInt(int[] bs){
@@ -93,36 +136,5 @@ public class SPN {
             i = i - ((i/2)+(i%2));
         }
         return bs;
-    }
-
-    // XOR values with subkey, r is round number
-    public int[][] subkeyMix(int[][] t, int r){
-
-        return t;
-    }
-
-    // substitutes a list of intermediate text inputs. Should be 16 in a [4][4] format
-    public int[][] substitue(int[][] t){
-        int[][] u = t;
-        int num;
-
-        // TRY / CATCH block for if there is an issue with mismatched substitution lengths and inputs.
-        try{
-            // iterates through each 'chunk' of 4 bits
-            for(int i = 0; i < t.length; i++){
-                num = this.substitutions[1][binaryToInt(t[i])];
-                u[i] = intToBinary(num);
-            }
-        } catch(Exception e){
-            // hopefully catches any issues with mismatched s-box lengths and inputs
-            System.out.println(e);
-        }
-        return u;
-    }
-
-    // permutes a list text inputs 
-    public int[][] permute(int[][] t){        
-
-        return t;
     }
 }
